1ea85ac9d1cd3c5fc7d0f41d1c8e41a5e7cafc11
Han Yoon <hbyoon1@g.ucla.edu>
2024-04-26 12:37:59 -0700

    check if cuda is available

--- visualizer.py
+++ visualizer.py
@@ -1,8 +1,14 @@
 import dental_env
 import gymnasium as gym
+import torch
 
-env = gym.make("DentalEnv3D-v1", render_mode="human", size=11)  # , render_mode="human"
+# print(torch.__version__)
+# print(f"Is CUDA supported by this system? {torch.cuda.is_available()}")
+# print(f"CUDA version: {torch.version.cuda}")
+
+env = gym.make("DentalEnv3D-v1", render_mode="human")  # , render_mode="human"
 observation, info = env.reset(seed=42)
+
 for _ in range(100):
     action = env.action_space.sample()  # this is where you would insert your policy
     observation, reward, terminated, truncated, info = env.step(action)

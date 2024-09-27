e1abe1dfab3e7d4adad5e3c1c0b1b42d14d90707
unknown <hbyoon1@g.ucla.edu>
2024-09-27 16:15:07 -0700

    Add voxel affine transformation

--- niblabel_test.py
+++ niblabel_test.py
@@ -2,7 +2,8 @@ import dental_env
 import open3d as o3d
 import numpy as np
 import nibabel as nib
-
+from scipy.ndimage import affine_transform
+from spatialmath import SO3, SE3
 
 def np_to_voxels(dict, arr):
     voxel_grid = o3d.geometry.VoxelGrid()
@@ -23,18 +24,22 @@ def np_to_voxels(dict, arr):
                 voxel_grid.add_voxel(voxel)
     return voxel_grid
 
-#
 img = nib.load('dental_env/labels/tooth_2.nii.gz')
 nparr = img.get_fdata()
 nparr = nparr[::5,::5,::5]
-nparr = np.rot90(nparr, k=1, axes=(0, 2))
+# nparr = np.rot90(nparr, k=1, axes=(0, 2))
+center = np.array(nparr.shape) / 2
 state = {
             "empty": 0,
             "decay": 1,
             "enamel": 2,
             "dentin": 3,
         }
+transform = SE3.Trans(center) * SE3.Rt(SO3.Rz(0, "deg"), [0, 0, 0]) * SE3.Trans(-center)
+nparr_transformed = affine_transform(nparr, transform.A, order=0)
 
 states_voxel = np_to_voxels(state, nparr)
+states_voxel_transformed = np_to_voxels(state, nparr_transformed)
 
-o3d.visualization.draw_geometries([states_voxel])
+frame = o3d.geometry.TriangleMesh.create_coordinate_frame(size=10)
+o3d.visualization.draw_geometries([states_voxel, states_voxel_transformed, frame])
